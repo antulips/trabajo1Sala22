73ecd2c700d6790313a5c70ab1d5db9a7e89e2bf
kushina <65832046+antulips@users.noreply.github.com>
2021-03-26 14:56:01 -0300

    [FIX] Resolví conflictos Karin

--- karin2.js
+++ karin2.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 console.log("¡Nos vemos!");
 console.log("¡Es magia!");
-=======
-console.log("¡Nos vemos más tarde por GitHub");
->>>>>>> c8169f4b8080d5b4a824800729d454e0e263dd6f
--- karin3.js
+++ karin3.js
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
-console.log("Hasta luego!");
-console.log("¡Es magia!");
-=======
 console.log("Hasta luego nos vemos en la nube!!!");
->>>>>>> c8169f4b8080d5b4a824800729d454e0e263dd6f
